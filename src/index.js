f1b9117ebdce324c13643d60875ef8c4aa9dde83
ashesbloom <mayankpandeydk123@gmail.com>
2024-09-05 22:31:16 +0530

    ..

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import * as serviceWorker from "./serviceWorker";
 import {
   Navigation,
